c1811d0b0d9887b19e827fe365cc973d90aad8ac
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-12-01 15:57:48 +0530

    simple web server in go

--- main.go
+++ main.go
@@ -1,43 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func helloHandler(resw http.ResponseWriter,req *http.Request){
-	if(req.URL.Path!="/hello"){
-		http.Error(resw,"404 not found",http.StatusNotFound)
-		return
-	}
-	if(req.Method!="GET"){
-		http.Error(resw,"Illegal method",http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(resw,"Hello there , this is what was supposed to be printed")
+func index(res http.ResponseWriter, req *http.Request){
+	fmt.Print(res,"GET request")
 }
 
-func formHandler(resw http.ResponseWriter,req *http.Request){
-	if(req.URL.Path!="/form"){
-		http.Error(resw,"404 not found",http.StatusNotFound)
-		return
-	}
-	name:=req.FormValue("name")
-	specialization:=req.FormValue("specialization")
-	fmt.Fprintf(resw,"Name = %s\nSpecialization = %s",name,specialization)
+func form(res http.ResponseWriter, req *http.Request){
+	fmt.Print(res,"POST request")
 }
 
-func main(){
-	fmt.Println("Server started at port 8080")
-	fileServer:=http.FileServer(http.Dir("./static"))
-	http.Handle("/",fileServer)
-	http.HandleFunc("/hello",helloHandler)
-	http.HandleFunc("/form",formHandler)
+func main() {
+	fileServer:= http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/index", index)
+	http.HandleFunc("/form", form)
 
-	err:=http.ListenAndServe(":8080",nil)
-	if(err!=nil){
+	fmt.Println("Server is running on port 5500")
+	if err:=http.ListenAndServe("127.0.0.1:5500",nil);err!=nil{
 		log.Fatal(err)
 	}
 }
-
